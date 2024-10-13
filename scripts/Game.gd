378645f724d068490c69e9e3c48172b9502289f4
david8zhang <david8zhang@berkeley.edu>
2024-10-13 12:29:26 -0700

    initial project setup

---
+++ scripts/Game.gd
@@ -0,0 +1,4 @@
+class_name Game
+extends Node2D
+
+@onready var player_manager: PlayerFieldManager = $PlayerFieldManager
---
+++ scripts/PlayerFieldManager.gd
@@ -0,0 +1,22 @@
+class_name PlayerFieldManager
+extends Node2D
+
+@export var player_scene: PackedScene
+var field_players = []
+var goalkeeper
+
+var player_configs = [
+	{
+		"position": Vector2(-400, -100)
+	},
+	{
+		"position": Vector2(-400, 100)
+	}
+]
+
+func _ready():
+	for config in player_configs:
+		var new_field_player = player_scene.instantiate() as FieldPlayer
+		new_field_player.global_position = config["position"]
+		field_players.append(new_field_player)
+		add_child(new_field_player)
